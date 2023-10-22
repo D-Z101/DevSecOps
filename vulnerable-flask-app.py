afe2d1ea8f7a446cb1a26e1680b0000872db3fb3
D-Z101 <119404494+D-Z101@users.noreply.github.com>
2023-10-23 00:33:03 +0200

    Update vulnerable-flask-app.py

--- vulnerable-flask-app.py
+++ vulnerable-flask-app.py
@@ -4,7 +4,7 @@ from werkzeug.datastructures import Headers
 from werkzeug.utils import secure_filename
 import sqlite3
 #fer
-#ze
+#er
 
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER']="/home/kali/Desktop/upload"
