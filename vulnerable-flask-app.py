7ccfb705fef1af8f0df1611216c0e667dfa2a81e
D-Z101 <119404494+D-Z101@users.noreply.github.com>
2023-10-23 01:49:37 +0200

    Update vulnerable-flask-app.py

--- vulnerable-flask-app.py
+++ vulnerable-flask-app.py
@@ -6,6 +6,7 @@ import sqlite3
 #twilight
 #Jacob
 #Edward
+#bella
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER']="/home/kali/Desktop/upload"
 app.config['MAX_CONTENT_LENGTH'] = 16 * 1000 * 1000
